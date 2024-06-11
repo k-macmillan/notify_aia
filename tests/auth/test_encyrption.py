c0350880e241e2e99e9c98fdc09e9b059627d1fe
Kyle MacMillan <kyle.w.macmillan@gmail.com>
2024-06-11 08:10:48 -0400

    Updated tests

--- tests/auth/test_encyrption.py
+++ tests/auth/test_encyrption.py
@@ -1,7 +1,8 @@
-from cryptography.fernet import Fernet
 import pytest
+from cryptography.fernet import Fernet
+from itsdangerous import URLSafeSerializer
 
-from naia.auth.encryption import decrypt, init_encryption
+import naia.auth.encryption as naia_encr
 
 
 def test_wb_init_encryption_valid_b64():
@@ -10,7 +11,7 @@ def test_wb_init_encryption_valid_b64():
         b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=',
         'Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc=',
     ]
-    init_encryption(keys)
+    naia_encr.init_encryption(keys)
 
 
 def test_wb_init_encryption_valid_legacy():
@@ -19,7 +20,7 @@ def test_wb_init_encryption_valid_legacy():
         b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=',
         'Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc=',
     ]
-    init_encryption(keys, b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=')
+    naia_encr.init_encryption(keys, b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=')
 
 
 def test_ut_init_encryption_invalid_too_short():
@@ -27,22 +28,22 @@ def test_ut_init_encryption_invalid_too_short():
         'Nzc3Nzc3Nzc3Nzc3Nzc3Nw==',
     ]
     with pytest.raises(ValueError):
-        init_encryption(not_long_enough)
+        naia_encr.init_encryption(not_long_enough)
 
 
 def test_ut_init_encryption_invalid_not_url_encoded():
     not_url_encoded = [
-        '&*?-'*8,
+        '&*?-' * 8,
     ]
     with pytest.raises(ValueError):
-        init_encryption(not_url_encoded)
+        naia_encr.init_encryption(not_url_encoded)
 
 
 def test_ut_init_encryption_invalid_not_iterable():
     # Valid key, but is not a list
     not_a_list = 'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY='
     with pytest.raises(TypeError) as exc:
-        init_encryption(not_a_list)
+        naia_encr.init_encryption(not_a_list)
     assert 'list' in str(exc)
 
 
@@ -52,7 +53,7 @@ def test_ut_init_encryption_invalid_legacy_type():
         'Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc=',
     ]
     with pytest.raises(TypeError):
-        init_encryption(keys, 1234)
+        naia_encr.init_encryption(keys, 1234)
 
 
 def test_wb_valid_decrypt_first_key():
@@ -60,22 +61,83 @@ def test_wb_valid_decrypt_first_key():
         b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=',
         'Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc=',
     ]
-    init_encryption(keys)
+    naia_encr.init_encryption(keys)
     # Encrypt using the key specified
     fk = Fernet(keys[0])
     test_str = 'primary key'
     enc_str = fk.encrypt(test_str.encode())
-    assert decrypt(enc_str) == test_str
+    assert naia_encr.decrypt(enc_str) == test_str
+
 
 def test_wb_valid_decrypt_old_key():
     keys = [
         b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=',
         'Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc=',
     ]
-    init_encryption(keys)
+    naia_encr.init_encryption(keys)
     # Encrypt using the key specified
     fk = Fernet(keys[1])
     test_str = 'old key'
     enc_str = fk.encrypt(test_str.encode())
-    assert decrypt(enc_str) == test_str
+    assert naia_encr.decrypt(enc_str) == test_str
 
+
+def test_wb_valid_legacy_key():
+    key = [
+        b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=',
+    ]
+    legacy_key = [
+        'Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc=',
+    ]
+    naia_encr.init_encryption(b64_keys=key, legacy_key=legacy_key)
+    serializer = URLSafeSerializer(secret_key=legacy_key)
+    test_str = 'The way to get started is to quit talking and begin doing'
+    signed = serializer.dumps(test_str)
+    assert naia_encr.legacy_verify(signed) == test_str
+
+
+def test_wb_valid_legacy_old_key():
+    key = [
+        b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=',
+    ]
+    legacy_keys = [
+        'Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc=',
+        b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=',
+    ]
+    # Test with one of the legacy keys removed
+    naia_encr.init_encryption(b64_keys=key, legacy_key=key)
+    # Representing a past init_encryption with both keys in use
+    serializer = URLSafeSerializer(secret_key=legacy_keys)
+    test_str = 'Well done is better than well said'
+    signed = serializer.dumps(test_str)
+    assert naia_encr.legacy_verify(signed) == test_str
+
+
+def test_ut_missing_legacy_key_for_legacy_verify(module_mocker):
+    key = [
+        b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=',
+    ]
+    naia_encr.init_encryption(b64_keys=key)
+    # Have to unset this due to other tests setting it
+    naia_encr._LEGACY_SERIALIZATION = None
+
+    with pytest.raises(RuntimeError) as exc:
+        naia_encr.legacy_verify('')
+    # Ensure user is pointed to init_encryption
+    assert 'init_encryption()' in str(exc)
+
+
+def test_ut_legacy_verify_invalid_signature():
+    key = [
+        b'YXNkZmFzZGZhc2RmYXNkZmFzZGZhc2RmYXNkZmFzZGY=',
+    ]
+    # Setup as if it was signed with a different key
+    old_key = 'Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc='
+    serializer = URLSafeSerializer(secret_key=old_key)
+    signed = serializer.dumps('Life is a succession of lessons which must be lived to be understood')
+
+    naia_encr.init_encryption(b64_keys=key, legacy_key=key)
+    with pytest.raises(ValueError) as exc:
+        naia_encr.legacy_verify(signed)
+    # Ensure user understands the validation failed
+    assert 'validation failed' in str(exc)
