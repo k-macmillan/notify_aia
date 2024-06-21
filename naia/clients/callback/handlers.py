2cb9b167f91d27aa1bbe1d064b459a3315c11c0b
Kyle MacMillan <kyle.w.macmillan@gmail.com>
2024-06-21 05:20:56 -0400

    Adjusted to handle HTTPExceptions

--- naia/clients/callback/handlers.py
+++ naia/clients/callback/handlers.py
@@ -1,9 +1,10 @@
 from time import monotonic
 from typing import Any, Callable, Coroutine, Union
 
-from fastapi import Request
+from fastapi import Request, status
+from fastapi.exceptions import HTTPException
 from fastapi.logger import logger
-from fastapi.responses import Response
+from fastapi.responses import JSONResponse, Response
 from fastapi.routing import APIRoute
 
 
@@ -12,7 +13,7 @@ class CallbackLoggingRoute(APIRoute):
         """Defines custom handling"""
         original_route_handler = super().get_route_handler()
 
-        async def custom_route_handler(request: Request) -> Union[Response, None]:
+        async def custom_route_handler(request: Request) -> Union[JSONResponse, Response, None]:
             """Handles route pre and post handling"""
             status_code = None
             resp = None
@@ -20,8 +21,14 @@ class CallbackLoggingRoute(APIRoute):
                 start = monotonic()
                 resp = await original_route_handler(request)
                 status_code = resp.status_code
+            except HTTPException as exc:
+                resp = JSONResponse(content={'error': exc.detail}, status_code=exc.status_code)
             except Exception as exc:
                 logger.critical('UNKNOWN EXCEPTION: %s %s', type(exc).__name__, exc)
+                resp = JSONResponse(
+                    content={'error': 'Unexpected error'},
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
             finally:
                 logger.info(f'{request.method} {request.url} {status_code} {monotonic() - start:6f}s')
             return resp
